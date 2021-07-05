fcc13ea4fb0357d67aad05c85413239b4282a570
Subin Min <tnqls5417@naver.com>
2021-07-05 23:25:59 +0900

    Add project

---
+++ src/main/java/gdghackathon/monthlychallenge/MonthlychallengeApplication.java
@@ -0,0 +1,13 @@
+package gdghackathon.monthlychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlychallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlychallengeApplication.class, args);
+	}
+
+}
